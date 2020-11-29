526a37d9248117fe867f5fda4fd029f55523d393
Akim Gubanov <akim.gubanov@gmail.com>
2020-11-30 02:55:12 +0300

    Some changes in guess-number

--- guess-number/main.go
+++ guess-number/main.go
@@ -5,46 +5,92 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"runtime"
 	"strconv"
 	"strings"
 	"time"
 )
 
-func main() {
-	min, max := 1, 100
-	rand.Seed(time.Now().UnixNano())
-	secretNumber := rand.Intn(max-min) + min
+const min int = 1
+const max int = 100
 
-	fmt.Println("Guess a number between 1 and 100")
-	fmt.Println("Please input your guess")
+var (
+	colorRed   string = "\033[31m"
+	colorGreen string = "\033[32m"
+	colorBlue  string = "\033[34m"
+	colorReset string = "\033[0m"
+)
+
+func main() {
+	printHeading()
+	play()
+}
+
+func printHeading() {
+	fmt.Print(`
+  ____                     _   _                 _                       _ 
+ / ___|_   _  ___  ___ ___| \ | |_   _ _ __ ___ | |__   ___ ____  __   _/ |
+| |  _| | | |/ _ \/ __/ __|  \| | | | | '_ ' _ \|  _ \ / _ \  __| \ \ / / |
+| |_| | |_| |  __/\__ \__ \ |\  | |_| | | | | | | |_) |  __/ |     \ V /| |
+ \____|\____|\___||___/___/_| \_|\__,_|_| |_| |_|____/ \___|_|      \_/ |_|
+`)
+
+	fmt.Println(colorBlue + `
+               GuessNumber v1.0 by Akim Gubanov (c) 2020                    
+` + colorReset)
+
+	return
+}
+
+func play() {
+	secretNumber := generateSecretNumber()
+
+	fmt.Println("> Greetings, stranger! I've just picked a number between 1 and 100. Try to guess it!")
+	fmt.Print("> Please input your guess: ")
 
 	attempts := 0
 	for {
 		attempts++
+
+		fmt.Print(colorGreen)
 		reader := bufio.NewReader(os.Stdin)
 		input, err := reader.ReadString('\n')
+		fmt.Print(colorReset)
 		if err != nil {
-			fmt.Println("An error occured while reading input. Please try again", err)
+			fmt.Println("An error occured while reading input. Please try again.", err)
 			continue
 		}
 
 		input = strings.TrimSuffix(input, "\n")
-
 		guess, err := strconv.Atoi(input)
 		if err != nil {
-			fmt.Println("Invalid input. Please enter an integer value")
+			fmt.Println("Invalid input. Please enter an integer value. By the way, you just failed one of your attempts.")
 			continue
 		}
 
-		fmt.Println("Your guess is", guess)
+		fmt.Printf("Your guess is %v. ", guess)
 
-		if guess > secretNumber {
-			fmt.Println("Your guess is bigger than the secret number. Try again")
-		} else if guess < secretNumber {
-			fmt.Println("Your guess is smaller than the secret number. Try again")
-		} else {
-			fmt.Println("Correct, you Legend! You guessed right after", attempts, "attempts")
-			break
+		switch {
+		case guess > secretNumber:
+			fmt.Print("It is bigger than the secret number. Try again: ")
+		case guess < secretNumber:
+			fmt.Print("It's smaller than the secret number. Try again: ")
+		default:
+			fmt.Printf("Correct, stranger! You guessed right after "+colorRed+"%v"+colorReset+" attempts.", attempts)
 		}
+	}
+}
+
+func generateSecretNumber() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min) + min
+}
+
+func init() {
+	if runtime.GOOS == "windows" {
+		colorReset = ""
+		colorRed = ""
+		colorGreen = ""
+		colorBlue = ""
 	}
 }
