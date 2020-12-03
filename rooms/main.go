78bb263846bba5c7ade30cddf0d30000bfe77a80
Akim Gubanov <akim.gubanov@gmail.com>
2020-12-04 01:58:02 +0300

    Changed one struct to slice

--- rooms/main.go
+++ rooms/main.go
@@ -11,45 +11,34 @@ var scanner *bufio.Scanner
 
 type storyNode struct {
 	text    string
-	choices *choices
+	choices []*choice
 }
 
-type choices struct {
+type choice struct {
 	cmd         string
 	description string
 	nextNode    *storyNode
-	nextChoice  *choices
 }
 
 func (node *storyNode) addChoice(cmd string, description string, nextNode *storyNode) {
-	choice := &choices{cmd, description, nextNode, nil}
-	if node.choices == nil {
-		node.choices = choice
-	} else {
-		currentChoice := node.choices
-		for currentChoice.nextChoice != nil {
-			currentChoice = currentChoice.nextChoice
-		}
-		currentChoice.nextChoice = choice
-	}
+	choice := &choice{cmd, description, nextNode}
+	node.choices = append(node.choices, choice)
 }
 
 func (node *storyNode) render() {
 	fmt.Println(node.text)
-	currentChoice := node.choices
-	for currentChoice != nil {
-		fmt.Printf("%s : %s\n", currentChoice.cmd, currentChoice.description)
-		currentChoice = currentChoice.nextChoice
+	if node.choices != nil {
+		for _, choice := range node.choices {
+			fmt.Printf("%s: %s\n", choice.cmd, choice.description)
+		}
 	}
 }
 
 func (node *storyNode) executeCmd(cmd string) *storyNode {
-	currentChoice := node.choices
-	for currentChoice != nil {
-		if strings.ToLower(currentChoice.cmd) == strings.ToLower(cmd) {
-			return currentChoice.nextNode
+	for _, choice := range node.choices {
+		if strings.ToLower(choice.cmd) == strings.ToLower(cmd) {
+			return choice.nextNode
 		}
-		currentChoice = currentChoice.nextChoice
 	}
 	fmt.Println("Sorry, I didn't understand that.")
 	return node
