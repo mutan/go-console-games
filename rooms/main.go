54c463bbb3f6cc1dc819dfe9ccd8b9bf1ca90c29
Akim Gubanov <akim.gubanov@gmail.com>
2020-12-04 01:33:11 +0300

    Changed rooms a bit

--- rooms/main.go
+++ rooms/main.go
@@ -9,6 +9,60 @@ import (
 
 var scanner *bufio.Scanner
 
+type storyNode struct {
+	text    string
+	choices *choices
+}
+
+type choices struct {
+	cmd         string
+	description string
+	nextNode    *storyNode
+	nextChoice  *choices
+}
+
+func (node *storyNode) addChoice(cmd string, description string, nextNode *storyNode) {
+	choice := &choices{cmd, description, nextNode, nil}
+	if node.choices == nil {
+		node.choices = choice
+	} else {
+		currentChoice := node.choices
+		for currentChoice.nextChoice != nil {
+			currentChoice = currentChoice.nextChoice
+		}
+		currentChoice.nextChoice = choice
+	}
+}
+
+func (node *storyNode) render() {
+	fmt.Println(node.text)
+	currentChoice := node.choices
+	for currentChoice != nil {
+		fmt.Printf("%s : %s\n", currentChoice.cmd, currentChoice.description)
+		currentChoice = currentChoice.nextChoice
+	}
+}
+
+func (node *storyNode) executeCmd(cmd string) *storyNode {
+	currentChoice := node.choices
+	for currentChoice != nil {
+		if strings.ToLower(currentChoice.cmd) == strings.ToLower(cmd) {
+			return currentChoice.nextNode
+		}
+		currentChoice = currentChoice.nextChoice
+	}
+	fmt.Println("Sorry, I didn't understand that.")
+	return node
+}
+
+func (node *storyNode) play() {
+	node.render()
+	if node.choices != nil {
+		scanner.Scan()
+		node.executeCmd(scanner.Text()).play()
+	}
+}
+
 func main() {
 	printHeading()
 	scanner = bufio.NewScanner(os.Stdin)
@@ -38,64 +92,6 @@ func main() {
 	start.play()
 
 	fmt.Println("The End.")
-}
-
-// storyNode
-
-type storyNode struct {
-	text    string
-	choices *choices
-}
-
-func (node *storyNode) addChoice(cmd string, description string, nextNode *storyNode) {
-	choice := &choices{cmd, description, nextNode, nil}
-	if node.choices == nil {
-		node.choices = choice
-	} else {
-		currentChoice := node.choices
-		for currentChoice.nextChoice != nil {
-			currentChoice = currentChoice.nextChoice
-		}
-		currentChoice.nextChoice = choice
-	}
-}
-
-func (node *storyNode) render() {
-	fmt.Println(node.text)
-	currentChoice := node.choices
-	for currentChoice != nil {
-		fmt.Printf("%s:%s\n", currentChoice.cmd, currentChoice.description)
-		currentChoice = currentChoice.nextChoice
-	}
-}
-
-func (node *storyNode) executeCmd(cmd string) *storyNode {
-	currentChoice := node.choices
-	for currentChoice != nil {
-		if strings.ToLower(currentChoice.cmd) == strings.ToLower(cmd) {
-			return currentChoice.nextNode
-		}
-		currentChoice = currentChoice.nextChoice
-	}
-	fmt.Println("Sorry, I didn't understand that.")
-	return node
-}
-
-func (node *storyNode) play() {
-	node.render()
-	if node.choices != nil {
-		scanner.Scan()
-		node.executeCmd(scanner.Text()).play()
-	}
-}
-
-// choices
-
-type choices struct {
-	cmd         string
-	description string
-	nextNode    *storyNode
-	nextChoice  *choices
 }
 
 func printHeading() {
