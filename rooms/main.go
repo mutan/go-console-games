0a06dc0d118b1506549a4ca1fda90730aee9a723
Akim Gubanov <akim.gubanov@gmail.com>
2020-12-05 02:58:32 +0300

    Some changes

--- rooms/main.go
+++ rooms/main.go
@@ -23,6 +23,7 @@ type room struct {
 	name        string
 	description string
 	moves       []*move
+	actions     []*action
 }
 
 type move struct {
@@ -31,9 +32,19 @@ type move struct {
 	nextRoom    *room
 }
 
+type action struct {
+	cmd         string
+	description string
+}
+
 func (room *room) addMove(cmd string, description string, nextRoom *room) {
 	move := &move{cmd, description, nextRoom}
 	room.moves = append(room.moves, move)
+}
+
+func (room *room) addAction(cmd string, description string) {
+	action := &action{cmd, description}
+	room.actions = append(room.actions, action)
 }
 
 func (room *room) removeMove(cmd string) {
@@ -46,17 +57,17 @@ func (room *room) removeMove(cmd string) {
 	room.moves = newMoves
 }
 
-func (room *room) render() {
-	fmt.Printf("Вы входите в комнату: %s.\n", room.name)
-	fmt.Println(room.description)
-	if room.moves != nil {
-		for _, move := range room.moves {
-			fmt.Printf("%s: %s\n", move.cmd, move.description)
+func (room *room) removeAction(cmd string) {
+	var newActions []*action
+	for _, action := range room.actions {
+		if action.cmd != cmd {
+			newActions = append(newActions, action)
 		}
 	}
+	room.actions = newActions
 }
 
-func (room *room) executeCmd(cmd string) *room {
+func (room *room) executeMoveCmd(cmd string) *room {
 	for _, move := range room.moves {
 		if strings.ToLower(move.cmd) == strings.ToLower(cmd) {
 			return move.nextRoom
@@ -66,12 +77,40 @@ func (room *room) executeCmd(cmd string) *room {
 	return room
 }
 
+/*func (room *room) executeActionCmd(cmd string) {
+	for _, action := range room.actions {
+		if strings.ToLower(action.cmd) == strings.ToLower(cmd) {
+			fmt.Println(action.description)
+			room.removeAction(cmd)
+		}
+	}
+	fmt.Println("Sorry, I didn't understand that.")
+
+}*/
+
+func (room *room) render() {
+	fmt.Printf("Вы входите в комнату: %s.\n", room.name)
+	fmt.Println(room.description)
+	if room.moves != nil {
+		fmt.Println("Движения:")
+		for _, move := range room.moves {
+			fmt.Printf("%s: %s\n", move.cmd, move.description)
+		}
+	}
+	if room.actions != nil {
+		fmt.Println("Действия:")
+		for _, action := range room.actions {
+			fmt.Printf("%s: %s\n", action.cmd, action.description)
+		}
+	}
+}
+
 func play(room *room) {
 	room.render()
 	if room.moves != nil {
 		scanner.Scan()
 		fmt.Println()
-		play(room.executeCmd(scanner.Text()))
+		play(room.executeMoveCmd(scanner.Text()))
 	}
 }
 
@@ -94,6 +133,7 @@ func main() {
 	hall.addMove("N", "На север", &darkRoom)
 	hall.addMove("S", "На юг", &darkRoom)
 	hall.addMove("E", "На восток", &trap)
+	hall.addAction("P", "Покопаться по карманам")
 
 	darkRoom.addMove("S", "Назад на юг", &grue)
 	darkRoom.addMove("O", "Включить фонарь", &darkRoomLit)
